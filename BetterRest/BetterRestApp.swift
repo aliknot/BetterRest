3c93d20fe3d1ac644df6f2c1327eb839096b6832
Alireza <alirezafazel379@gmail.com>
2024-12-18 10:00:23 +0100

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Alireza Fazel on 18/12/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
